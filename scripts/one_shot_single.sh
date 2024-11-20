ee6c55aecdc697ab273fa0c36cf2e4400be32df2
Derkai <2363104914@qq.com>
2024-11-20 10:32:31 +0800

    [style]: 文本优化.

--- scripts/one_shot_single.sh
+++ scripts/one_shot_single.sh
@@ -7,14 +7,8 @@ if [ $? -eq 0 ]
 then
     echo "[Drone id]: $drone_id"
     echo "start global interface"
-    gnome-terminal -- bash -c "source devel/setup.bash;roslaunch global_interface driver_start.launch drone_id:=$drone_id" && sleep 1;
+    source devel/setup.bash;roslaunch global_interface driver_start.launch drone_id:=$drone_id && sleep 1;
     sleep 3;
-
-    # # 开启视觉里程计 VINS
-    # gnome-terminal -- bash -c "source devel/setup.bash;roslaunch vins swarm_d430_no_rviz.launch"  && sleep 1;
-
-    # # 开启规控算法 Ego # TODO(Derkai): 需要条件编译,不然就和ego-planner的包名重复导致无法通过编译检查
-    # bash ./ipc_run.sh && sleep 1;
 
 else
     echo "[error]Not found drone_id,please_check params_file"
